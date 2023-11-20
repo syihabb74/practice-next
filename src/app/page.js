f2c2b17fdd8699aa1919287d31d1aa883360dbbf
syihabb74 <syihabb74@gmail.com>
2023-11-20 08:20:36 +0700

    lanjutan

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,17 @@
 import Image from 'next/image'
+import {Header} from "@/components/header";
+import {Content} from "@/components/content";
+import {Footer} from "@/components/footer";
 
 export default function Home() {
-  return <div></div>
+  
+  return (<div>
+  <Header />
+  <Content name="Syihab" />
+  <Footer />
+  </div>)
+  
+
+
 }
+
---
+++ src/components/header.jsx
@@ -0,0 +1,3 @@
+export const Header = () => {
+    return <header className="bg-red-500 text-red">Header</header>
+}
